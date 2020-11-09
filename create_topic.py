17c2f5a4904a46915499822bd523881cd7a8265c
Jon Staley <jon.staley@beryl.cc>
2020-11-09 15:33:40 +0000

    check if host is up before creating topics

--- create_topic.py
+++ create_topic.py
@@ -1,4 +1,8 @@
+import os
+import time
+import socket
 # copied from here https://github.com/googleapis/python-pubsub/blob/master/samples/snippets/publisher.py#L43
+
 
 def create_topic(project_id, topic_id):
     from google.cloud import pubsub_v1
@@ -7,12 +11,29 @@ def create_topic(project_id, topic_id):
     topic_path = publisher.topic_path(project_id, topic_id)
     topic = publisher.create_topic(request={"name": topic_path})
 
-if __name__ == "__main__":
-    import os
 
+def is_open(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect((ip, int(port)))
+        s.shutdown(2)
+        return True
+    except:
+        return False
+
+
+if __name__ == "__main__":
     project_id = os.environ["PUBSUB_PROJECT_ID"]
     # topics should be ':' separated
     topics = os.environ["PUBSUB_TOPICS"]
 
-    for topic in topic.split(":"):
+    host, port = os.environ["PUBSUB_EMULATOR_HOST"].split(":")
+
+    while not is_open(host, port):
+        print("Waiting for pubsub ...")
+        time.sleep(1)
+
+    for topic in topics.split(":"):
         create_topic(project_id, topic)
+        print(f"created topic: {topic}")
